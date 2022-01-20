8f175b77567a448966e71135920595a6d050513a
Sara Vieira <sara.vieira@remote.com>
2022-01-20 14:31:44 +0000

    shuffle sharks

--- src/All.js
+++ src/All.js
@@ -2,12 +2,32 @@ import { Link, useParams } from 'react-router-dom'
 import sharks from './sharks.json'
 import { gradients } from './utils/gradients'
 
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+
+    // And swap it with the current element.
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex]
+    ]
+  }
+
+  return array
+}
+
 const All = () => {
   var background = gradients[Math.floor(Math.random() * gradients.length)]
 
   return (
     <section style={{ background }} className="shark-wrapper all">
-      {sharks.map((shark) => (
+      {shuffle(sharks).map((shark) => (
         <div>
           <div>
             <img className="shark" src={`${shark.image}.jpg`} />
