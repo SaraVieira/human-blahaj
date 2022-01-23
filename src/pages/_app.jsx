2dd2d1764e460cd7f1e8ebe96ce3594032786d94
Sara Vieira <hey@iamsaravieira.com>
2022-01-23 14:40:50 +0100

    add usefathom

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,30 @@
 import Header from '@/config'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as Fathom from 'fathom-client'
+
 import '@/styles/index.css'
 
 function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('FSVYGODH', {
+      includedDomains: ['blahaj.xyz'],
+    })
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview()
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
   return (
     <>
       <Header />
